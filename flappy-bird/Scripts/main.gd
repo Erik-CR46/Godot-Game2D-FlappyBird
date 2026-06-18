05bd088a8acf0499740db44d37be36233a38b682
Erik-CR46 <erikcatalanrodriguez@gmail.com>
2026-06-18 22:27:38 +0200

    Suelo en bucle

--- flappy-bird/Scripts/main.gd
+++ flappy-bird/Scripts/main.gd
@@ -6,10 +6,8 @@ extends Node2D
 func _on_player_on_game_started() -> void: #Llamamos a la señal on_game_started (se añade player porque viene de esa clase)
 	spawner.timer.start()
 	
-func _on_obstacle_player_crashed() -> void:
-	print("colision")
-
-
 func _on_spawner_on_obstacle_crash() -> void:
 	player.stop_movement()
 	
+func _on_ground_on_player_crash() -> void:
+	spawner.stop_obstacles()
